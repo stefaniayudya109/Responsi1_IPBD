88dc34370d9713d06f1f1d677abc0deeba84513c
stefaniayudya109 <stefaniayudya05@gmail.com>
2026-04-23 21:14:10 +0700

    ubah dag

--- scrape.py
+++ scrape.py
@@ -9,54 +9,54 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 
-options = Options()
-options.page_load_strategy = 'eager'
+def get_data():
+    options = Options()
+    options.page_load_strategy = 'eager'
+    options.add_argument("--headless")  # WAJIB untuk Airflow
 
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()),
-    options=options
-)
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
 
-driver.get("https://www.wired.com/category/business/")
-time.sleep(5)
+    driver.get("https://www.wired.com/category/business/")
+    time.sleep(5)
 
-# scroll secukupnya aja (jangan kebanyakan biar gak error)
-for i in range(5):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)
+    for i in range(3):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
 
-articles = driver.find_elements(By.CSS_SELECTOR, "a[href*='/story/']")
+    articles = driver.find_elements(By.CSS_SELECTOR, "a[href*='/story/']")
 
-data = []
-seen = set()
+    data = []
+    seen = set()
 
-for a in articles:
-    try:
-        url = a.get_attribute("href")
-        title = a.text.strip()
-    except:
-        continue
+    for a in articles:
+        try:
+            url = a.get_attribute("href")
+            title = a.text.strip()
+        except:
+            continue
 
-    if not title or not url or url in seen:
-        continue
+        if not title or not url or url in seen:
+            continue
 
-    seen.add(url)
+        seen.add(url)
 
-    data.append({
-        "title": title,
-        "url": url,
-        "description": None,
-        "author": None,
-        "scraped_at": datetime.now().isoformat(),
-        "source": "Wired.com"
-    })
+        data.append({
+            "title": title,
+            "url": url,
+            "scraped_at": datetime.now().isoformat(),
+            "source": "Wired.com"
+        })
 
-    if len(data) >= 50:
-        break
+        if len(data) >= 50:
+            break
 
-driver.quit()
+    driver.quit()
 
-with open("wired_data.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, indent=4, ensure_ascii=False)
+    # optional save file
+    with open("wired_data.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
 
-print("Jumlah data:", len(data))
+    return data
--- wired_dag.py
+++ wired_dag.py
@@ -2,49 +2,35 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
+from scrape import get_data
+from insert_data import insert_to_db
 
-def fetch_and_insert():
-    import requests
-    import psycopg2
 
-    res = requests.get("http://host.docker.internal:8000/articles")
-    data = res.json()["data"]
+def scrape_task(ti):
+    data = get_data()
+    ti.xcom_push(key="scraped_data", value=data)
 
-    conn = psycopg2.connect(
-        host="host.docker.internal",
-        port=5433,
-        database="wired_db",
-        user="admin",
-        password="admin"
-    )
 
-    cur = conn.cursor()
-
-    for item in data:
-        cur.execute("""
-            INSERT INTO wired_articles (title, url, description, author, scraped_at)
-            VALUES (%s, %s, %s, %s, %s)
-        """, (
-            item["title"],
-            item["url"],
-            item["description"],
-            item["author"],
-            item["scraped_at"]
-        ))
-
-    conn.commit()
-    cur.close()
-    conn.close()
+def insert_task(ti):
+    data = ti.xcom_pull(task_ids='scrape', key='scraped_data')
+    insert_to_db(data)
 
 
 with DAG(
     dag_id="wired_pipeline",
     start_date=datetime(2024, 1, 1),
-    schedule=None,
+    schedule="@daily",
     catchup=False
 ) as dag:
 
-    task = PythonOperator(
-        task_id="fetch_and_insert_data",
-        python_callable=fetch_and_insert
+    scrape = PythonOperator(
+        task_id="scrape",
+        python_callable=scrape_task
     )
+
+    insert = PythonOperator(
+        task_id="insert",
+        python_callable=insert_task
+    )
+
+    scrape >> insert
